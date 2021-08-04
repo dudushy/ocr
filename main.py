b1dd0a77faacc77ff8a0d6ba5d5af12271bd6f03
dudushy <eduardotalarico1337@gmail.com>
2021-08-04 15:13:43 -0300

    working demo

--- main.py
+++ main.py
@@ -4,3 +4,12 @@ from pytesseract import *
 
 # init
 pytesseract.tesseract_cmd = r'C:\Users\User\AppData\Local\Programs\Tesseract-OCR\tesseract.exe'
+
+# vars
+img = Image.open(r'ocr\input\demo.png')
+output = pytesseract.image_to_string(img)
+
+print(output)
+
+with open(r'ocr\output\demo.txt', 'w', encoding='utf-8') as result:
+    result.write(output)
