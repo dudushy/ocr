b39beb8b51874af2ead1c486526581eed6b69ac8
dudushy <eduardotalarico1337@gmail.com>
2021-08-04 16:09:52 -0300

    before test

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 # imports
 from PIL import Image
 from pytesseract import *
+import glob
 
 # init
 pytesseract.tesseract_cmd = r'C:\Users\User\AppData\Local\Programs\Tesseract-OCR\tesseract.exe'
 
-# vars
+# main 1
 img = Image.open(r'ocr\input\demo.png')
 output = pytesseract.image_to_string(img)
 
@@ -13,3 +14,11 @@ print(output)
 
 with open(r'ocr\output\demo.txt', 'w', encoding='utf-8') as result:
     result.write(output)
+
+# main 2
+try:                       #0123456789-4321
+    for img in glob.iglob(r'ocr\input\*.png'):
+        with open(r'ocr\output\test.txt', 'a', encoding='utf-8') as output:
+            output.write(pytesseract.image_to_string(img))
+except Exception as e:
+    print('\n' + e)
